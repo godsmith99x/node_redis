148f53b1eb2a72a0b2ca5bcf70ee25ea3ea801e2
godsmith <jgodfrey-smith@pm.me>
2021-10-11 15:49:58 -0400

    Updating docker-compose.yml

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+    //___             _                  _  _    _                               
+   /// _ \  ___    __| | ___  _ __ ___  (_)| |_ | |__                                  
+  /// /_\/ / _ \  / _` |/ __|| '_ ` _ \ | || __|| '_ \                                       
+ /// /_\\ | (_) || (_| |\__ \| | | | | || || |_ | | | |                                      
+ //\____/  \___/  \__,_||___/|_| |_| |_||_| \__||_| |_|                                      
+                         
+
 const express = require('express');
 const redis = require('redis');
 
 
 const app = express();
 const client = redis.createClient();
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
@@ -13,5 +21,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(8081, () => {
+    console.log('Listening on port 8081');
 
 })
